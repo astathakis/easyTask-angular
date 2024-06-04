1b06e541247966fce114c81719bb256e395ec64e
tasos <tasos_stathakis@yahoo.com>
2024-06-05 02:43:08 +0300

    addition of models

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,6 +7,14 @@ import {
   input,
 } from '@angular/core';
 
+import { type User } from './user.model';
+
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -15,9 +23,7 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
   // avatar = input.required<string>();
   // name = input.required<string>();
 
@@ -26,10 +32,10 @@ export class UserComponent {
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
